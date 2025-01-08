d3c6147331e0a2a7307912115b8082dd5b36c544
Daniel <danstraws@gmail.com>
2025-01-08 13:27:38 -0600

    changed behavior to avoid connection when the mongoDB URI is not configured

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,8 +1,10 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
 
 export default mongoose.connection;
